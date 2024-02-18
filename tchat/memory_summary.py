8a95356886294b88d60dedc1dd10876bf09a729e
Nygil Pereira <nygilgp@gmail.com>
2024-02-18 11:54:41 +0530

    chat with memory

---
+++ tchat/memory_summary.py
@@ -0,0 +1,38 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.memory import ConversationSummaryMemory, ConversationBufferMemory, FileChatMessageHistory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI(verbose=True)
+
+memory = ConversationSummaryMemory(
+  memory_key="messages", 
+  return_messages=True,
+  #chat_memory=FileChatMessageHistory("messages.json")
+  llm=chat
+)
+
+prompt = ChatPromptTemplate(
+  input_variables=["content", "messages"],
+  messages=[
+    MessagesPlaceholder(variable_name="messages"),
+    HumanMessagePromptTemplate.from_template("{content}")
+  ]
+)
+
+chain = LLMChain(
+  llm = chat,
+  prompt = prompt,
+  memory=memory,
+  verbose=True
+)
+
+while True:
+  content = input(">> ")
+  
+  result = chain({"content": content})
+  
+  print(result["text"])
